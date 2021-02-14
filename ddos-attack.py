90b0f9e2f901762f585933a6729457fc1207d141
subhankar-12-web <78792665+subhankar-12-web@users.noreply.github.com>
2021-02-14 23:21:04 +0530

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -20,7 +20,7 @@ bytes = random._urandom(1490)
 os.system("clear")
 os.system("figlet DDos Attack")
 print
-print "Author   : HA-MRX"
+print "Author   : subhankar
 print "You Tube : https://www.youtube.com/c/HA-MRX"
 print "github   : https://github.com/Ha3MrX"
 print "Facebook : https://www.facebook.com/muhamad.jabar222"
